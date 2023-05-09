a95ed450897ea93530a296cd2ddec8ad9f63daeb
GolovinAndrei <golovinandraha@protonmail.com>
2023-05-09 14:41:49 +0300

    my parseInt

--- HTML_CSS_JS/app.js
+++ HTML_CSS_JS/app.js
@@ -1,74 +1,52 @@
-//console.log("Hello world!")
-
-// let a = "5";
-// a = a +5;
-
-// console.log(typeof a);
-
-// a-=5;
-// console.log(typeof a);
-
-// a=a+"0";
-// console.log(typeof a);
-
-// a=+a;
-// console.log(typeof a);
-// console.log(a);
-
-// if (a=0){
-//     console.log("true");
-// }
-// console.log(a);
-
-// let a = "abc";
-// let b = 2;
-// let c = 3;
-
-// if (a < c){
-//     console
-//     .log("yes");
-// }
-// a=!a;console.log(a)
-// console.log()
-// let g = new Number(9);
-// console.log(g)
-// g=+g
-// console.log(g)
-
-// let str = "256 * 320";
-// console.log(str)
-// console.log(+str)
-// console.log(parseInt(str))
-
-// let w = "546.8u";
-// console.log(+w, parseInt(w), parseFloat(w))
-// console.log(w.length)
 
 function myToString(number, radix){
-    let res= number<0 ? "-" : "";
+    let sign= number<0 ? "-" : "";
     number = Math.abs(number.toFixed(0));
     if (radix<2 || radix>36) {
         radix = 10;
     }
-    let pref = 0;
-    if (radix>10) {
-        pref = 55;
-    }
-    let resArr = [];
-    let i = 0;
+    let res = "";
    do{
-       let res = number%radix;
-        if (res>9) {
-            res = String.fromCharCode(pref+res);
+       let digit = number%radix;
+        if (digit>9) {
+            digit = String.fromCharCode(55+digit);
         }
-        resArr[i] = res;
-        i++;
+        res = digit+res;
         number= Number.parseInt(number/radix);
    } while(number!=0);
-   for (let i = resArr.length-1; i>=0; i--){
-        res+=resArr[i];
-   }
+  res = sign+res;
    return res; 
 }
 
-console.log(myToString(-45586666, 1));
+//console.log(myToString(-123456789, 36));
+
+
+function myParseInt (number, radix){
+    let  res = NaN;
+    if (radix>=2 && radix<=36) {
+        res=0;
+    let sign= 1;
+    let i = 0;
+    let encodeArr = number.split('.')[0].split("");
+    if (encodeArr[0]=='-'){
+        sign = -1;
+        i = 1
+    }
+    const pref1 = '0'.charCodeAt(0);
+    const pref2 = 'A'.charCodeAt(0)-10;
+    let run = true;
+    while (run && i<encodeArr.length) {
+        let digit = encodeArr[i].charCodeAt(0);
+        digit = digit>pref1+9 ? digit-pref2:digit-pref1;
+        if (digit<radix) {
+            res = res*radix + digit;
+            i++;
+        } else run=!run;
+    }
+    res*=sign; 
+}
+    return res;
+}
+    
+ console.log(myParseInt('A16', 16));
+

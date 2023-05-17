206a48eca79505b5a6b8aa56bdf4aeb604ea5bf0
GolovinAndrei <golovinandraha@protonmail.com>
2023-05-17 15:25:00 +0300

    overriding of arrays methods

--- HTML_CSS_JS/app.js
+++ HTML_CSS_JS/app.js
@@ -39,18 +39,77 @@ class Square extends Rectangle{
 }
 
 const square = new Square(10);
-console.log(square.perimeter());
-console.log(square.square());
-console.log(square);
+// console.log(square.perimeter());
+// console.log(square.square());
+// console.log(square);
 
-Array.prototype.map = function(){
-    console.log('fsfsfsfsdfsdf');
+
+
+Array.prototype.myForEach  = function(func){
+    for (let i=0; i<this.length; i++){
+        func(this[i], i, this);
+    }
 }
 
-Array.prototype.reduce = function(){
-    console.log('fsfsfsfsdfsdf');
+arr.myForEach(n=>console.log(n));
+
+Array.prototype.myMap  = function(func){
+    let res=[];
+    for (let i=0; i<this.length; i++){
+        res[i]=func(this[i], i, this);
+    }
+    return res;
 }
 
-Array.prototype.forEach = function(){
-    this.map()
+console.log(arr.myMap(n=>n+1));
+
+Array.prototype.myFilter  = function(func){
+    let res=[];
+
+    for (let i=0; i<this.length; i++){
+        if (func(this[i], i, this)) {
+            res.push(this[i]);
+        }
+    }
+    return res;
 }
+console.log([1,2,3,4,5,6].myFilter(n=>n>4));
+
+Array.prototype.myReduce  = function(func, start){
+    let res =start == undefined ? this[0] : start;
+    let i =0;
+    if (start==undefined) {i=1;}
+    for (i; i<this.length; i++){
+        res=func(res, this[i], i, this);
+    }
+    return res;
+}
+console.log([1,2,3,4,5,6].myReduce((a,b)=>a+b));
+
+
+// class Deffered {
+//     constructor (a){
+//         this.a = a;
+
+//     }
+// }
+
+
+
+// const d = new Deferred();
+
+// d.then(function(res){ 
+//     console.log("1 ", res); 
+//     return "a"; 
+// });
+
+// d.then(function(res){
+//      console.log("2 ", res); 
+//      return "b"; 
+//     });
+
+// d.then(function(res){ console.log("3 ", res); return "c"; });
+// d.resolve('hello');
+
+
+
